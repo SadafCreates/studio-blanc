bedb5a5cc42b961f140ce3124dcc0b23b6d330c5
Sadaf Farooq <xadaffarooq@gmail.com>
2026-08-30 23:12:57 +0500

    Added real downloaded pics

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,12 @@
 export const collection = [
-  { num: '01', name: 'The Wool Coat', category: 'Outerwear', image: "https://i0.wp.com/kuku.pk/wp-content/uploads/2025/11/bw25-1.jpg?resize=800%2C1067&ssl=1" },
-  { num: '02', name: 'Ceramic Vessel', category: 'Home', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsmh-xF2VVoIn3C2fvDr9vA-cHiabTO8KUiCzCaP8vtA&s' },
-  { num: '03', name: 'Silk Scarf', category: 'Accessories', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN0MTSVPTBkysUlGuxofk-wSuNVbJNcoWmCAvZjzZ8zw&s=10' },
-  { num: '04', name: 'Leather Tote', category: 'Bags', image: 'https://aodour.pk/cdn/shop/files/5_yxnWH5.jpg?format=webp&v=1775849286&width=800' },
+  { num: '01', name: 'The Wool Coat', category: 'Outerwear', image: "images/coat.jpeg" },
+  { num: '02', name: 'Ceramic Vessel', category: 'Home', image: 'images/download.jpeg' },
+  { num: '03', name: 'Silk Scarf', category: 'Accessories', image: 'images/scarf.jpeg' },
+  { num: '04', name: 'Leather Tote', category: 'Bags', image: 'images/bag.jpeg' },
 ];
 
 export const lookbook = [
-  { caption: 'Studio, morning light', image: 'https://img.peerspace.com/image/upload/f_auto,q_auto,dpr_auto,w_3840/x2oijeba83j0hcjc14q0' },
-  { caption: 'The rail', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8Arcb4oJEUTD5YroFOAq6Zll6D0XghYBoxnSnUbtBvFOPjZC_8fuJqW9c&s=10' },
-  { caption: 'Detail', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIm57nPsGk40kIPZs-eiYBAaEN7g4NMHwe-5KxlpgUu0jhKYYC9dLlWE3j&s=10' },
+  { caption: 'Studio, morning light', image: 'images/morning light.jpeg' },
+  { caption: 'The rail', image: 'images/rail.jpeg' },
+  { caption: 'Detail', image: 'images/imdetails.jpeg' },
 ];
